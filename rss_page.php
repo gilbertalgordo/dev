3ab83e0d082763c9e67b39cb4182992a631c843c
Gilbert Algordo <69397216+gilbertalgordo@users.noreply.github.com>
2024-02-17 15:26:12 +0800

    Add files via upload
    Weather Kit
    
    Signed-off-by: Gilbert Algordo <69397216+gilbertalgordo@users.noreply.github.com>

---
+++ rss_page.php
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="ISO-8859-1" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+        <atom:link href="https://www.weather.gov/rss_page.php?site_name=nws" rel="self" type="application/rss+xml" />
+        <title>National Weather Service</title>
+        <link>http://www.weather.gov</link>
+        <description>Latest News Headlines</description>
+        <item>
+            <title>Heavy Rains and High Elevation Snow in the West; Lafe-Effect Snow Along the Great Lakes</title>
+            <link>www.wpc.ncep.noaa.gov/discussions/hpcdiscussions.php?disc=pmdspd</link>
+            <guid>https://www.weather.gov/nws/20240216ah01</guid>
+            <pubDate>Sat, 17 Feb 2024 02:09:36 +0000</pubDate>
+        </item>
+        <item>
+            <title>Dangerous Heat in the Southern U.S. This Week; Heavy Rains in Portions of the Southwest and Central U.S.</title>
+            <link>www.wpc.ncep.noaa.gov/discussions/hpcdiscussions.php?disc=pmdspd</link>
+            <guid>https://www.weather.gov/nws/20230731ah01</guid>
+            <pubDate>Tue, 01 Aug 2023 01:54:36 +0000</pubDate>
+        </item>
+        <item>
+            <title>Areas of Severe Thunderstorms, Excessive Rainfall, and Heat into the Weekend</title>
+            <link>www.wpc.ncep.noaa.gov/discussions/hpcdiscussions.php?disc=pmdspd</link>
+            <guid>https://www.weather.gov/nws/20230621ss01</guid>
+            <pubDate>Wed, 21 Jun 2023 12:09:07 +0000</pubDate>
+        </item>
+    </channel>
+</rss>
